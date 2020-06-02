27d7d9657ffac1f8f099e406c35c8156b1086193
Dominic Chell <dmchell@gmail.com>
2020-06-02 15:51:20 +0100

    First push

---
+++ Sniper/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+namespace Sniper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Process[] processlist = Process.GetProcesses();
+            foreach (Process theprocess in processlist)
+            {
+                    try
+                    {
+                        ProcessModuleCollection myProcessModuleCollection = theprocess.Modules;
+                        ProcessModule myProcessModule;
+
+                        for (int i = 0; i < myProcessModuleCollection.Count; i++)
+                        {
+                            myProcessModule = myProcessModuleCollection[i];
+                            if (myProcessModule.ModuleName.Contains("clr.dll"))
+                            {
+
+                                Console.WriteLine("######### Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
+
+                                Console.WriteLine("The moduleName is "
+                                    + myProcessModule.ModuleName);
+                                Console.WriteLine("The " + myProcessModule.ModuleName + "'s base address is: "
+                                    + myProcessModule.BaseAddress);
+                                Console.WriteLine("The " + myProcessModule.ModuleName + "'s Entry point address is: "
+                                    + myProcessModule.EntryPointAddress);
+                                Console.WriteLine("The " + myProcessModule.ModuleName + "'s File name is: "
+                                    + myProcessModule.FileName);
+                                i = myProcessModuleCollection.Count;
+                                Scanner.MemScan(theprocess.ProcessName);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("!!!!!!!! Unable to Access Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
+                        //Console.WriteLine(e.Message);
+                    }
+                }
+            Console.WriteLine("Complete...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sniper/Scanner.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Sniper
+{
+    class Scanner
+    {
+        static Byte[] peHeader = new Byte[] { 0x4D, 0x5A, 0x90, 0x00, 0x03, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x0E, 0x1F, 0xBA, 0x0E, 0x00, 0xB4, 0x09, 0xCD, 0x21, 0xB8, 0x01, 0x4C, 0xCD, 0x21, 0x54, 0x68, 0x69, 0x73, 0x20, 0x70, 0x72, 0x6F, 0x67, 0x72, 0x61, 0x6D, 0x20, 0x63, 0x61, 0x6E, 0x6E, 0x6F, 0x74, 0x20, 0x62, 0x65, 0x20, 0x72, 0x75, 0x6E, 0x20, 0x69, 0x6E, 0x20, 0x44, 0x4F, 0x53, 0x20, 0x6D, 0x6F, 0x64, 0x65 };
+        static Byte[] peEnd = new byte[] { 0x3C, 0x2F, 0x61, 0x73, 0x73, 0x65, 0x6D, 0x62, 0x6C, 0x79, 0x3E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xE0, 0x0B, 0x00, 0x0C, 0x00, 0x00, 0x00, 0x10, 0x37 };
+
+        const int PROCESS_QUERY_INFORMATION = 0x0400;
+        const int MEM_COMMIT = 0x00001000;
+        const int PAGE_EXECUTE_READ = 0x20;
+        const int PAGE_EXECUTE_READWRITE = 0x40;
+        const int PAGE_READWRITE = 0x04;
+        const int PROCESS_WM_READ = 0x0010;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern UIntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+
+
+        [DllImport("KERNEL32.DLL", SetLastError = true)]
+        public static extern bool ReadProcessMemory(
+            UIntPtr process, ulong address, byte[] buffer, ulong size, ref uint read);
+
+        [DllImport("kernel32.dll")]
+        static extern void GetSystemInfo(out SYSTEM_INFO lpSystemInfo);
+
+        [DllImport("KERNEL32.DLL")]
+        public static extern int VirtualQueryEx(UIntPtr hProcess, UIntPtr lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, int dwLength);
+
+        public struct MEMORY_BASIC_INFORMATION
+        {
+            public ulong BaseAddress;
+            public ulong AllocationBase;
+            public uint AllocationProtect;
+            public uint __alignment1;
+            public ulong RegionSize;
+            public uint State;
+            public uint Protect;
+            public uint Type;
+            public uint __alignment2;
+        }
+
+        public struct SYSTEM_INFO
+        {
+            public ushort processorArchitecture;
+            ushort reserved;
+            public uint pageSize;
+            public UIntPtr minimumApplicationAddress;
+            public UIntPtr maximumApplicationAddress;
+            public UIntPtr activeProcessorMask;
+            public uint numberOfProcessors;
+            public uint processorType;
+            public uint allocationGranularity;
+            public ushort processorLevel;
+            public ushort processorRevision;
+        }
+
+        public static IntPtr _Scan(byte[] sIn, byte[] sFor)
+        {
+            int[] sBytes = new int[256]; int Pool = 0;
+            int End = sFor.Length - 1;
+            for (int i = 0; i < 256; i++)
+                sBytes[i] = sFor.Length;
+            for (int i = 0; i < End; i++)
+                sBytes[sFor[i]] = End - i;
+            while (Pool <= sIn.Length - sFor.Length)
+            {
+                for (int i = End; sIn[Pool + i] == sFor[i]; i--)
+                    if (i == 0) return new IntPtr(Pool);
+                Pool += sBytes[sIn[Pool + End]];
+            }
+            return IntPtr.Zero;
+        }
+        
+        public static void MemScan(string processName)
+        {
+            SYSTEM_INFO sys_info = new SYSTEM_INFO();
+            GetSystemInfo(out sys_info);
+
+            UIntPtr proc_min_address = sys_info.minimumApplicationAddress;
+            UIntPtr proc_max_address = sys_info.maximumApplicationAddress;
+
+            ulong proc_min_address_l = (ulong)proc_min_address;
+            ulong proc_max_address_l = (ulong)proc_max_address;
+
+            Process process = Process.GetProcessesByName(processName)[0];
+
+            UIntPtr processHandle =
+            OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_WM_READ, false, (uint)process.Id);
+
+            MEMORY_BASIC_INFORMATION mem_basic_info = new MEMORY_BASIC_INFORMATION();
+
+            uint bytesRead = 0;
+
+            while (proc_min_address_l < proc_max_address_l)
+            {
+                VirtualQueryEx(processHandle, proc_min_address, out mem_basic_info, Marshal.SizeOf(typeof(MEMORY_BASIC_INFORMATION)));
+
+                if (((mem_basic_info.Protect == PAGE_EXECUTE_READWRITE) || (mem_basic_info.Protect == PAGE_EXECUTE_READ)) && mem_basic_info.State == MEM_COMMIT)
+                {
+                    byte[] buffer = new byte[mem_basic_info.RegionSize];
+
+                    ReadProcessMemory(processHandle,
+                    mem_basic_info.BaseAddress, buffer, mem_basic_info.RegionSize, ref bytesRead);
+                    IntPtr Result = _Scan(buffer, peHeader);
+                    if (Result != IntPtr.Zero)
+                    {
+                        Console.WriteLine("!!! Found PE binary in region: 0x{0}, Region Sz 0x{1}", (mem_basic_info.BaseAddress).ToString("X"), (mem_basic_info.RegionSize).ToString("X"));
+                        Console.WriteLine("!!! Carving PE from memory...");
+                        using (FileStream fileStream = new FileStream("out.exe", FileMode.Create))
+                        {
+                            for (uint i = (uint)Result; i < mem_basic_info.RegionSize; i++)
+                            {
+                                fileStream.WriteByte(buffer[i]);
+                            }
+                        }
+                    }
+                }
+
+                proc_min_address_l += mem_basic_info.RegionSize;
+                proc_min_address = new UIntPtr(proc_min_address_l);
+            }
+        }
+    }
+}
